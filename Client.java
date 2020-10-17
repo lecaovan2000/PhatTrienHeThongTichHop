3adce35bb86df52b243458788d1a897038fcf1ba
lecaovan2000 <72064491+lecaovan2000@users.noreply.github.com>
2020-10-17 23:51:51 +0700

    Câu 5

--- Client.java
+++ Client.java
@@ -1,30 +1,33 @@
-	package ClientRMI;
-	import java.io.IOException;
-	import java.io.PrintStream;
-	import java.net.Socket;
-	import java.net.UnknownHostException;
-	import java.util.Scanner;
-	public class Client {
-	
-		public static void main(String[] args) {
-			try {
-				Socket client = new Socket("LocalHost",88);
-				System.out.println("Client");
-				Scanner inFromServer = new Scanner(client.getInputStream());
-				PrintStream outToServer = new PrintStream(client.getOutputStream());
-				System.out.println("server: " + inFromServer.nextLine());
-				Scanner scan = new Scanner(System.in);
-				String tens = scan.nextLine();
-				outToServer.println(tens);
-				System.out.println("server: " + inFromServer.hasNext());
-				} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
+package DaoChuoi;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+				try {
+					Socket client = new Socket("LocalHost", 88);
+					System.out.println("Client da duoc tao");
+					Scanner inFromServer = new Scanner(client.getInputStream());
+					PrintStream outToServer = new PrintStream(client.getOutputStream());
+					System.out.println("server: " + inFromServer.nextLine());
+					Scanner scan = new Scanner(System.in);
+					String a = scan.nextLine();
+					outToServer.println(a);
+					System.out.println("server: " + inFromServer.nextLine());
+					}
+				catch (UnknownHostException e) 
+					{
+						e.printStackTrace();
+					}
+				catch (IOException e) {
 					e.printStackTrace();
-				} catch (IOException e) {
-			// TODO Auto-generated catch block
-					e.printStackTrace();
-			}
-	
-		}
-	
+					}
 	}
+
+
+}
